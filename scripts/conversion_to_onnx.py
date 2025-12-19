e051fb3a04f50b36246eea57488e17115d54e7c3
OgrodowskiJedrzej <jedrekogro@gmail.com>
2025-12-19 23:48:23 +0100

    Fiix to onnx export.

--- scripts/conversion_to_onnx.py
+++ scripts/conversion_to_onnx.py
@@ -1,9 +1,10 @@
 import torch
 
 from src.utils import load_model
+from src.config import cfg
 
 model = load_model(
     "models/wavegram_logmel.pth", model_type="wavegram_logmel")
-example_input = (torch.randn(1,32000*5),)
-onnx_model = torch.onnx.export(model, example_input, dynamo=True)
+example_input = (torch.randn(1,32000*cfg["training"]["interval"]),)
+onnx_model = torch.onnx.export(model, example_input)
 onnx_model.save("models/wavegram_logmel.onnx")
